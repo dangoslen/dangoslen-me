9dfe03155622c778018c6b7c129d51798aec5fdc
Dan Goslen <dwgoslen@gmail.com>
2017-03-18 21:41:55 -0400

    Finishing up rough draft one. Working email controller using Formspree

--- resources/js/dangoslen-me.js
+++ resources/js/dangoslen-me.js
@@ -1,37 +1,19 @@
 var application = angular.module('dangoslen-me', []);
 
-application.directive('myDescription', ['$interval', function($interval) {
-    return {
-        template: '<span class="description">{<code>{{description}}</code>}</span>',
-        link: function update(scope) {
-            var index = 1;
-            var descriptions = ['Software Engineer', 'Coffee Nerd', 'Outdoor Enthusiast', 'Musician', 'Ultimate Frisbee Player', 'Fan of Animated Movies', 'Follower of Jesus'];
-
-            scope.description = descriptions[0];
-
-            function updateDescription(update) {
-                if(index == descriptions.length) {
-                    index = 0;
-                 }
-
-                scope.description = descriptions[index];
-
-                index++;
-            }
-
-            update = $interval(function() {
-                updateDescription();
-            }, 2000, descriptions.length);
-          }
-        };
-    }]);
-
 application.controller('EmailMeController', ['$scope', '$http', function($scope, $http) {
+      $scope.$email_response = {success: false, sent: false, text: "Haven't sent yet"};
+
       $scope.sendEmail = function() {
           $http({
             method: "POST",
             url: "https://formspree.io/dan@dangoslen.me",
             data: { email: emailMeForm.email.value, name: emailMeForm.name.value, message: emailMeForm.message.value }
+          }).then(function($response) {
+              if($response.status == 200) {
+                $scope.$email_response = {success: true, sent: true, text: "Thanks for sending me an email! I'll get back to you soon."};
+              }
+          }, function($error) {
+              $scope.$email_response = {success: false, sent: true, text: "Ruh-roh! Something went wrong. Try again!"};
           });
         };
     }]);
