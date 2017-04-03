e087c0b6fa0a4af620ab2cffab9c61ab942ccf2f
Dan Goslen <dwgoslen@gmail.com>
2017-04-02 20:47:44 -0400

    Removing unnecesary dependencies and re-working build to work with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "resources/css/style.css": "resources/less/style.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['resources/less/style.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
